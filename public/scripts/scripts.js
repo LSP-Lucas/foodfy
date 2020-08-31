38910f028359934ad79ad640610cddd17b8b45f3
Lucas Pedroso <lucassppedroso@gmail.com>
2020-08-31 16:44:55 -0300

    criação de input dinâmico

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -4,6 +4,8 @@ const list__preparation = document.querySelector('.list-preparation');
 const description = document.querySelector('.description');
 const admin__recipe = document.querySelectorAll('#show-recipe');
 
+const btn_edit = document.querySelector('#btn-edit');
+
 // Admin
 
 for (let i = 0; i < admin__recipe.length; i++) {
@@ -15,12 +17,50 @@ for (let i = 0; i < admin__recipe.length; i++) {
     });
 }
 
-document.querySelector('#btn-edit').addEventListener("click", function() {
+if(btn_edit) {
 
-    const url = window.location.href.split("/");
+    btn_edit.addEventListener("click", function() {
 
-    window.location.href = `/admin/recipes/${url[url.length - 1]}/edit`;
+        const url = window.location.href.split("/");
+    
+        window.location.href = `/admin/recipes/${url[url.length - 1]}/edit`;
+    
+    });
 
+}
+
+document.querySelector(".add-ingredient").addEventListener("click", function () {
+
+    const ingredients = document.querySelector("#ingredients");
+    const fieldContainer = document.querySelectorAll(".ingredient");
+
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    ingredients.appendChild(newField);
+    
+});
+
+document.querySelector(".add-preparation-mode").addEventListener("click", function () {
+
+    const preparation = document.querySelector("#preparation");
+    const fieldContainer = document.querySelectorAll(".preparation-mode");
+
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    preparation.appendChild(newField);
+    
 });
 
 
@@ -75,5 +115,9 @@ document.querySelector('#btn-information').addEventListener("click", function ()
         document.getElementById('btn-information').innerHTML = "MOSTRAR";
     }
 });
+
+
+
+  
 
 
